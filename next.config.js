f2ad1d36d34e852e783cf332c7095848d2edf499
Yan Goncalves <fgyan94@gmail.com>
2023-10-12 16:02:25 -0300

    refactor: next config env

--- next.config.js
+++ next.config.js
@@ -9,8 +9,11 @@ module.exports = withBundleAnalyzer({
   },
   serverRuntimeConfig: {
     NEXT_PUBLIC_LAUNCHES_API: process.env.NEXT_PUBLIC_LAUNCHES_API,
+  },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_LAUNCHES_API: process.env.NEXT_PUBLIC_LAUNCHES_API,
     NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
     NEXT_PUBLIC_EXPERIMENT_ID: process.env.NEXT_PUBLIC_EXPERIMENT_ID,
     NEXT_PUBLIC_OPTIMIZE_ID: process.env.NEXT_PUBLIC_OPTIMIZE_ID,
-  }
+  },
 });
