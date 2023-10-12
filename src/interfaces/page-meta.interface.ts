06a7751773060d75f2857c6ad381d0371f9a481c
Yan Goncalves <fgyan94@gmail.com>
2023-10-12 10:55:54 -0300

    feat: add page dtos interfaces

---
+++ src/interfaces/page-meta.interface.ts
@@ -0,0 +1,10 @@
+export interface IPageMeta {
+  totalDocs: number;
+  limit: number;
+  totalPages: number;
+  page: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: number;
+  nextPage: number;
+}
---
+++ src/interfaces/page.ts
@@ -0,0 +1,3 @@
+export interface IPage<T> {
+  docs: T[];
+}
