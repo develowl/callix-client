072fc05564c472eb2af2d77afc2116800997ec26
Yan Goncalves <fgyan94@gmail.com>
2023-10-13 00:26:27 -0300

    refactor: page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider defaultColorScheme={'dark'} theme={theme}>
       <Head>
-        <title>Mantine Template</title>
+        <title>Launches - Callix Client</title>
         <meta
           name={'viewport'}
           content={'minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no'}
